ed8e46ba97d282e7932dfad4c79a5207bd882582
Priyanshu <106882732+PriyanshuY21@users.noreply.github.com>
2024-07-10 23:52:43 +0530

    0

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
+const { TbH2 } = require('react-icons/tb');
+
 /** @type {import('tailwindcss').Config} */
   module.exports = {
   content: ["./index.html","./src/**/*.{html,js,jsx,tsx}"],
   theme: {
+    screens:{
+      t1:"1122px",
+       t2:"758px",
+     },
+     
     extend: {
+      screens:{
+       lg:"1008px",
+      },
       colors:{
         primary: "#D9D9D9",
         secondary: "#FFFFFF",
